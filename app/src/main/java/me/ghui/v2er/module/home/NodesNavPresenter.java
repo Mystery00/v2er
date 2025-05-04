4508f4299f44d77221578623e508444a75320c99
Mystery0 <mystery0dyl520@gmail.com>
2025-05-04 18:40:35 +0800

    feat: remove node items that contain "•" in the category

--- app/src/main/java/me/ghui/v2er/module/home/NodesNavPresenter.java
+++ app/src/main/java/me/ghui/v2er/module/home/NodesNavPresenter.java
@@ -1,5 +1,8 @@
 package me.ghui.v2er.module.home;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
 import me.ghui.v2er.network.APIService;
 import me.ghui.v2er.network.GeneralConsumer;
 import me.ghui.v2er.network.bean.NodesNavInfo;
@@ -23,6 +26,7 @@ public class NodesNavPresenter implements NodesNavConstract.IPresenter {
                 .subscribe(new GeneralConsumer<NodesNavInfo>(mView) {
                     @Override
                     public void onConsume(NodesNavInfo items) {
+                        items.removeIf(it -> it.getCategory().contains("•"));
                         mView.fillView(items);
                     }
                 });
